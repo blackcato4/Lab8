60825ba94a97a7eb187ffde9176468c46bada0bf
Phakbung <athikan_thoyaem@cmu.ac.th>
2025-12-17 08:14:48 +0700

    lab8_3 completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,4 @@
+
 /*
 "Enter your age: "
 "Enter your height: "
@@ -11,3 +12,42 @@
 "Brook"
 "Jinbe"
 */
+#include<iostream>
+using namespace std ;
+int main(){
+int age;
+double bounty ;
+float height ;
+cout << "Enter your age: " ;
+cin >> age ;
+if(age <= 25){
+    cout << "Enter your height: " ;
+    cin >> height ;
+    if(height < 100){
+        cout << "Your character = ""Chopper""" << endl ;
+    }else if(height < 180){
+        cout << "Your character = ""Usopp""" << endl ;
+    }else{
+        cout << "Enter your bounty: " ;
+        cin >> bounty ;
+        if(bounty > 1100000000){
+            cout << "Your character = ""Zoro""" << endl ;
+        }else{
+            cout << "Your character = ""Sanji""" << endl ;
+        }
+    }
+
+}else if(age <= 60){
+    cout << "Enter your bounty: " ;
+    cin >> bounty ;
+    if(bounty > 500000000){
+        cout << "Your character = ""Jinbe""" << endl ;
+    }else{
+        cout << "Your character = ""Franky""" << endl ;
+    }
+}else{
+    cout << "Your character = ""Brook""" << endl;
+}
+return 0;
+}
+
